5a18da6531a21644300109a9d8b01af576ff8e51
Hongting Zhu <hongtingzhu@Hongtings-MBP.wireless.yale.internal>
2024-01-31 14:04:32 -0500

    nav bar

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
 var images = ["images/01526.jpg","images/rotozazaetude.jpg"]
-
 var imgState = 0;
-
 var imgTag = document.getElementById("frontImage");
-
-console.log(imgTag);
 
 imgTag.addEventListener("click", function (event) {
   imgState = (++imgState % images.length);
   event.target.src = images[imgState];
+});
+
+var topnav = document.getElementById("topNav");
+var hoverSpace = document.getElementById("hoverspace");
+console.log(hoverSpace);
+hoverSpace.addEventListener("mousemove", function(event) {
+  if(100 - event.clientY < 0) {
+    $(topnav).css({top: 100 - event.clientY});
+  } else {
+    $(topnav).css({top: 0});
+  }
+});
+
+hoverSpace.addEventListener("mouseout", function() {
+  $(topnav).css({top: -100});
 });
