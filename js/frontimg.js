702682da43be8437f8d1184ae7c3a49aed8a63e2
Iviaaa <hongtingzhu@Hongtings-MBP.hsd1.ct.comcast.net>
2024-02-29 22:53:09 -0500

    add materials for 10 pages

--- js/frontimg.js
+++ js/frontimg.js
@@ -1,8 +1,28 @@
-var images = ["images/01526.jpg","images/lot.jpeg"]
+var images = [
+            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_1.jpg",
+            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_2.jpg",
+            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_3.jpg",
+            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_4.jpg",
+            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_5.jpg",
+            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_6.jpg",
+            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_7.jpg",
+            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_8.jpg",
+            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_9.jpg",
+            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_10.jpg",
+            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_11.jpg",
+            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_12.jpg",
+            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_13.jpg",
+            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_14.jpg"]
 var imgState = 0;
 var imgTag = document.getElementById("frontImage");
 
 imgTag.addEventListener("click", function (event) {
-  imgState = (++imgState % images.length);
+  if (event.clientX > window.innerWidth/2) {
+    imgState = (++imgState % images.length);
+  }
+  else{
+    imgState = ((--imgState % images.length) + images.length) % images.length;
+  }
+  console.log(imgState);
   event.target.src = images[imgState];
 });
