fa3c39cfb1ff953ad6578afa9416321dd9c19c0e
Iviaaa <hongtingzhu@Hongtings-MBP.hsd1.ct.comcast.net>
2024-03-05 15:17:17 -0500

    change nav hovertext

--- js/frontimg.js
+++ js/frontimg.js
@@ -1,17 +1,7 @@
 var images = [
-            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_1.jpg",
-            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_2.jpg",
-            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_3.jpg",
-            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_4.jpg",
-            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_5.jpg",
-            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_6.jpg",
-            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_7.jpg",
-            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_8.jpg",
-            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_9.jpg",
-            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_10.jpg",
-            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_11.jpg",
-            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_12.jpg",
-            "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_13.jpg",
+            "images/ascii-art (33).png",
+            "images/615dVi-uiCL.jpg",
+            "images/b91fe0f9d08e5a978f7663bb5a01a054.gif",
             "images/1709245105808-dee558ca-0970-4df1-bc79-64396bd36d2b_14.jpg"]
 var imgState = 0;
 var imgTag = document.getElementById("frontImage");
